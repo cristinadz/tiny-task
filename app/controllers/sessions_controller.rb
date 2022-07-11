360b6c7055e39a03cc2566605d6f57220751e3e3
Mai Vang <vmaineng@gmail.com>
2022-07-11 10:47:48 -0700

    sessions controller for create route

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+    
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user.id] = user.id
+            render json: user, status: :created
+        else
+            render json: {error: "Invalid"}, status: :unprocessable_entity
+        end
+    end
+
+end
