e740c8ad3c5b819f5d922f1ede868f2cdb8c0c25
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-07-11 15:50:04 -0700

    routing added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from '../pages/Login';
 import NavBar from './NavBar';
-
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     // auto-login
-    fetch("/me").then((r) => {
+    fetch('/me').then((r) => {
       if (r.ok) {
         r.json().then((user) => setUser(user));
       }
@@ -19,9 +19,13 @@ function App() {
   if (!user) return <Login onLogin={setUser} />;
 
   return (
-    <div className="App">
-      <NavBar user = {user} setUser= {setUser} />
-      <Login />
+    <div className='App'>
+      <NavBar user={user} setUser={setUser} />
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='login' element={<Login onLogin={setUser} />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,61 +1,56 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 function LoginForm({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
 
-  function handleSubmit(e){
-      e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
 
-      fetch("/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({username, password}),
-        }).then((r) => {
-          if (r.ok) {
-            r.json().then((user) => onLogin(user));
-          } else {
-            r.json().then((err) => setErrors(err.errors));
-          }
-        });
-
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
   }
 
   return (
-      <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
+      <label name='username'>Username</label>
+      <input
+        type='text'
+        id='username'
+        autoComplete='off'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
 
-          <label name="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            autoComplete="off"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+      <label name='password'>Password</label>
+      <input
+        type='password'
+        id='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        autoComplete='current-password'
+      />
 
-          <label name="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password"
-          />
+      <button type='submit'>Login</button>
 
-          <button type="submit">Login</button>
-      
-          {/* {errors.map((err) => (
+      {errors.map((err) => (
         <p key={err}>{err}</p>
-          ))}
-      */}
+      ))}
+    </form>
+  );
+}
 
-      </form>
-    );
-  }
-
-
-
-export default LoginForm
+export default LoginForm;
