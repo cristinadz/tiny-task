46853596aa965644c26eadb7a2b155b760e64882
Cristina Diaz <diazcristina.la@gmail.com>
2022-07-12 09:29:32 -0700

    create

--- app/controllers/favorited_activities_controller.rb
+++ app/controllers/favorited_activities_controller.rb
@@ -1,3 +1,10 @@
 class FavoritedActivitiesController < ApplicationController
+    
+    def create 
+        activity = Activity.find_by(id: params[:id])
+        favorite = FavoritedActivity.create( activity_id: activity.id, user_id: @current_user.id, completed: false )
+        render json: favorite, status: :created
+    end
 
 end
+
