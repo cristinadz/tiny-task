d89f12ef0a062ac12177debe519415b12ca7e3d2
Cristina Diaz <diazcristina.la@gmail.com>
2022-07-14 08:51:27 -0700

    login and navbar styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,25 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
-function NavBar({user, setUser}) {
-
-    function handleLogoutClick() {
-        fetch("/logout", { method: "DELETE" }).then((r) => {
-          if (r.ok) {
-            setUser(null);
-          }
-        });
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Center } from '@chakra-ui/react';
+function NavBar({ user, setUser }) {
+  function handleLogoutClick() {
+    fetch('/logout', { method: 'DELETE' }).then((r) => {
+      if (r.ok) {
+        setUser(null);
       }
-    
-
+    });
+  }
   return (
-    <div>
-      <nav> 
-      
-        <Link to="/home">Home</Link> 
-        <Link to="/favorites">Favorites</Link> 
-        <Link to="/profile">Profile</Link> 
-        <button onClick={handleLogoutClick}>
-          Logout
-        </button>
+    <Center bg='teal' h='100px' color='white'>
+      <nav>
+        <Link to='/home'>Home</Link>
+        <Link to='/favorites'>Favorites</Link>
+        <Link to='/profile'>Profile</Link>
+        <button onClick={handleLogoutClick}>Logout</button>
       </nav>
-    </div>
-  )
+    </Center>
+  );
 }
+export default NavBar;
 
-export default NavBar
+
