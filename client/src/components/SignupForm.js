fd75eeddb9831090da803b8ccabd0d5aaaa26e94
Jenna Hrdlicka <hrdlickajenna@gmail.com>
2022-07-15 11:16:03 -0700

    last-min styling and background image fix

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   Button,
-Box,
+  Box,
   Flex,
   FormControl,
   FormLabel,
@@ -14,13 +14,13 @@ Box,
   Alert,
   AlertIcon,
   AlertDescription,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
 function SignupForm({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [errors, setErrors] = useState([]);
   const navigate = useNavigate();
 
@@ -28,10 +28,10 @@ function SignupForm({ onLogin }) {
     e.preventDefault();
     setErrors([]);
 
-    fetch("/signup", {
-      method: "POST",
+    fetch('/signup', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username,
@@ -47,63 +47,67 @@ function SignupForm({ onLogin }) {
           r.json().then((err) => setErrors(err.errors));
         }
       })
-      .then(navigate("/home"));
+      .then(navigate('/home'));
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <Stack direction={{ base: "column", md: "row" }}>
-        <Flex flex={1} align={"center"} justify={"center"}>
-        <Box
-          bg={useColorModeValue("orange.50", "orange.400")}
-          boxShadow={"lg"}
-          rounded={'xl'}
-          p={4}
-        >
-          <Stack spacing={4} w={"full"} maxW={"md"}>
-            <Heading fontSize={"2xl"}>Sign in to your account</Heading>
-            <FormControl id="new_username">
-              <FormLabel>Username</FormLabel>
-              <Input
-                type="text"
-                id="new_username"
-                autoComplete="off"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </FormControl>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input
-                type='text'
-                    id='email'
-                    autoComplete='off'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-              />
+      <Stack direction={{ base: 'column', md: 'row' }}>
+        <Flex flex={1} align={'center'} justify={'center'}>
+          <Box
+            bg={useColorModeValue('orange.50', 'orange.400')}
+            boxShadow={'lg'}
+            rounded={'xl'}
+            p={4}
+          >
+            <Stack spacing={3} mx={'auto'} maxW={'md'}>
+              <Stack align={'4'}>
+                <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+                  Create an account
+                </Heading>
+              </Stack>
+              <FormControl id='new_username'>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type='text'
+                  id='new_username'
+                  autoComplete='off'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input
-                type="password"
-                id="new_password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                autoComplete="current-password"
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Password Confirmation</FormLabel>
-              <Input
-                type="password"
-                id="password_confirmation"
-                value={passwordConfirmation}
-                onChange={(e) => setPasswordConfirmation(e.target.value)}
-                autoComplete="current-password"
-              />
-            </FormControl>
-            <Stack spacing={6}>
-              {/* <Stack
+              <FormControl id='email'>
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type='text'
+                  id='email'
+                  autoComplete='off'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl id='password'>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type='password'
+                  id='new_password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete='current-password'
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Password Confirmation</FormLabel>
+                <Input
+                  type='password'
+                  id='password_confirmation'
+                  value={passwordConfirmation}
+                  onChange={(e) => setPasswordConfirmation(e.target.value)}
+                  autoComplete='current-password'
+                />
+              </FormControl>
+              <Stack spacing={6}>
+                {/* <Stack
                 direction={{ base: "column", sm: "row" }}
                 align={"start"}
                 justify={"space-between"}
@@ -111,19 +115,19 @@ function SignupForm({ onLogin }) {
                 <Checkbox>Remember me</Checkbox>
                 <Link color={"blue.500"}>Forgot password?</Link>
               </Stack> */}
-              <Button colorScheme={"blue"} variant={"solid"} type="submit">
-                Sign in
-              </Button>
-              <Alert status="error" borderRadius={4}>
-                <AlertIcon />
-                <AlertDescription>
-                  {errors.map((err) => (
-                    <p>{err}</p>
-                  ))}
-                </AlertDescription>
-              </Alert>
+                <Button colorScheme={'blue'} variant={'solid'} type='submit'>
+                  Sign in
+                </Button>
+                <Alert status='error' borderRadius={4}>
+                  <AlertIcon />
+                  <AlertDescription>
+                    {errors.map((err) => (
+                      <p>{err}</p>
+                    ))}
+                  </AlertDescription>
+                </Alert>
+              </Stack>
             </Stack>
-          </Stack>
           </Box>
         </Flex>
       </Stack>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,65 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import {
-	Heading,
-	Avatar,
-	Box,
-	Center,
-	Text,
-	Button,
-	useColorModeValue,
-} from "@chakra-ui/react";
+  Heading,
+  Avatar,
+  Box,
+  Center,
+  Text,
+  Button,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 function Profile({ user }) {
-	const { profile_img, username, email } = user;
+  const { profile_img, username, email } = user;
 
-	return (
-		<Center py={80}>
-			<Box
-				maxW={"320px"}
-				w={"full"}
-				bg={useColorModeValue("white", "green")}
-				boxShadow={"base"}
-				rounded={"xl"}
-				p={6}
-				textAlign={"center"}
-			>
-				<Avatar
-					size={"2xl"}
-					src={profile_img}
-					alt={"Avatar Alt"}
-					mb={4}
-					pos={"relative"}
-					boxShadow={"base"}
-				/>
-				<Heading fontSize={"2xl"} fontFamily={"body"}>
-					{username}
-				</Heading>
-				<Text fontWeight={600} color={"gray.500"} mb={4}>
-					{email}
-				</Text>
+  return (
+    <Center py={10}>
+      <Box
+        maxW={'320px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'green')}
+        boxShadow={'base'}
+        rounded={'xl'}
+        p={6}
+        textAlign={'center'}
+      >
+        <Avatar
+          size={'2xl'}
+          src={profile_img}
+          alt={'Avatar Alt'}
+          mb={4}
+          pos={'relative'}
+          boxShadow={'base'}
+        />
+        <Heading fontSize={'2xl'} fontFamily={'body'}>
+          {username}
+        </Heading>
+        <Text fontWeight={600} color={'gray.500'} mb={4}>
+          {email}
+        </Text>
 
-				<Button
-					as={Link}
-					to="/edit_profile"
-					flex={1}
-					fontSize={"sm"}
-					rounded={"full"}
-					bg={"green.400"}
-					color={"white"}
-					boxShadow={
-						"0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
-					}
-					_hover={{ bg: "green.500" }}
-					_focus={{
-						bg: "green.500",
-					}}
-				>
-					edit
-				</Button>
-			</Box>
-		</Center>
-	);
+        <Button
+          as={Link}
+          to='/edit_profile'
+          flex={1}
+          fontSize={'sm'}
+          rounded={'full'}
+          bg={'green.400'}
+          color={'white'}
+          boxShadow={
+            '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+          }
+          _hover={{ bg: 'green.500' }}
+          _focus={{
+            bg: 'green.500',
+          }}
+        >
+          edit
+        </Button>
+      </Box>
+    </Center>
+  );
 }
 
 export default Profile;
