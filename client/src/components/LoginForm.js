b9546a7f1ec301ba2ba9b3fc3c13aa50fcae51f1
Mai Vang <vmaineng@gmail.com>
2022-07-15 10:45:35 -0700

    login an signup completed

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,14 +6,14 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Checkbox,
   Stack,
-  VStack,
   Link,
   Button,
   Heading,
   useColorModeValue,
-  Alert, AlertIcon, AlertDescription 
+  Alert,
+  AlertIcon,
+  AlertDescription,
 } from "@chakra-ui/react";
 
 function LoginForm({ onLogin }) {
@@ -36,7 +36,8 @@ function LoginForm({ onLogin }) {
         if (r.ok) {
           r.json().then((user) => onLogin(user));
         } else {
-          r.json().then((err) => setErrors(err.errors));
+          r.json().then((err) => setErrors(err.errors))
+          console.log(errors);
         }
       })
       .then(navigate("/home"));
@@ -44,78 +45,73 @@ function LoginForm({ onLogin }) {
 
   return (
     <Flex
-    // minH={'100vh'}
       align={"top"}
-      // justify={"top"}
-      py={-100}
+      justify={"top"}
+      pt={0}
       flex={1}
     >
-      <Stack spacing={3} mx={"auto"} maxW={"md"} p={6} my={12}>
+      <Stack spacing={3} mx={"auto"} maxW={"md"} >
         <Stack align={"4"}>
-          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>Log in to your account</Heading>
+          <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+            Log in to your account
+          </Heading>
         </Stack>
         <Box
-          rounded={"sm"}
           bg={useColorModeValue("orange.50", "orange.400")}
           boxShadow={"lg"}
+          rounded={'xl'}
           p={4}
         >
-          <Stack spacing={2} >
-          <form onSubmit={handleSubmit}>
-            <FormControl id="username" isRequired >
-              <FormLabel>Username</FormLabel>
-              <Input
-                type="text"
-                id="username"
-                autoComplete="off"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </FormControl>
-            <FormControl id="password" isRequired mt={4}>
-              <FormLabel>Password</FormLabel>
-              <Input 
-              type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="current-password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: "column", sm: "row" }}
-                align={"start"}
-                justify={"space-between"}
-              >
-                <Checkbox>Remember me</Checkbox>
-                <Link color={"blue.400"}>Forgot password?</Link>
+          <Stack spacing={2}>
+            <form onSubmit={handleSubmit}>
+              <FormControl id="username" isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type="text"
+                  id="username"
+                  autoComplete="off"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </FormControl>
+              <FormControl id="password" isRequired mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="current-password"
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  type="submit"
+                >
+                  Log in
+                </Button>
+                { errors ? 
+                (<Box my={4}>
+                  <Alert status="error" borderRadius={4}>
+                    <AlertIcon />
+                    <AlertDescription>
+                      {errors.map((err) => (
+                        <p>{err}</p>
+                      ))}
+                    </AlertDescription> 
+                  </Alert>
+                </Box>) : null }
               </Stack>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-                type="submit"
-              >
-                Log in
-              </Button>
-              <Box my={4}>
-                
-              <Alert status="error" borderRadius={4}>
-              <AlertIcon />
-             <AlertDescription>{errors.map((err) => (
-            <p>{err}</p>
-          ))}</AlertDescription> 
-          </Alert>
-         
-          </Box>
-            </Stack>
             </form>
           </Stack>
         </Box>
       </Stack>
-    // </Flex>
+    </Flex>
   );
 }
 
